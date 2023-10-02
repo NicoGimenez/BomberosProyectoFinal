93ea58f926fd4789f526e8fb636587df7964e955
LEsayago <lucas.e.sayago@gmail.com>
2023-10-02 20:26:18 -0300

    comit

--- src/bomberosgrupo56/BomberosGrupo56.java
+++ src/bomberosgrupo56/BomberosGrupo56.java
@@ -1,29 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bomberosgrupo56;
 
-/**
- *
- * @author niqog
- */
 public class BomberosGrupo56 {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+   
     }
-    //
-    
-    
-    
-    
-    
-    
-    
+
    
 }
