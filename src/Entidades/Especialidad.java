f11c8780ef5dc46042f55b9778681be42d28c5c3
LEsayago <lucas.e.sayago@gmail.com>
2023-10-03 18:48:21 -0300

    especialidad

---
+++ src/Entidades/Especialidad.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Lucas E. Sayago
+ */
+public enum Especialidad {
+
+    INCENDIOS("Incendios en viviendas e industrias"),
+    DERRUMBES("Salvamento en derrumbes"),
+    RESCATE_MONTAÑA("Rescates en ámbito montaña"),
+    RESCATE_TRAFICO("Rescate de personas atrapadas en accidentes de tráfico"),
+    INUNDACIONES("Socorrer inundaciones"),
+    PREVENCION("Operativos de prevención");
+
+    private final String descripcion;
+
+    Especialidad(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
