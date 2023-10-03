60ab4599df88d6ae928d45f77183ffc35f16c2d9
LEsayago <lucas.e.sayago@gmail.com>
2023-10-03 18:32:01 -0300

    cuartel

--- src/Entidades/Cuartel.java
+++ src/Entidades/Cuartel.java
@@ -1,9 +1,19 @@
 
 package Entidades;
 
+import java.util.ArrayList;
+
 /*
  * @author Lucas E. Sayago
  */
 public class Cuartel {
-
+    
+    private int codigoCuartel;
+    private String nombreDeCuartel;
+    private String direccion;
+    private int coordenadaEnX;
+    private int coordenadaEnY;
+    private String telefono;
+    private  String correo;
+    private ArrayList<Brigada> brigadas;
 }
