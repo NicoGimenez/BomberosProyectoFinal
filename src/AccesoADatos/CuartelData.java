62b5ece9ff60d77752957fffc3a91c41cc1cf8bf
LEsayago <lucas.e.sayago@gmail.com>
2023-10-03 19:46:28 -0300

    conexion cuartel

--- src/AccesoADatos/CuartelData.java
+++ src/AccesoADatos/CuartelData.java
@@ -1,9 +1,17 @@
 
 package AccesoADatos;
 
+import java.sql.Connection;
+
 /*
  * @author Lucas E. Sayago
  */
 public class CuartelData {
+    
+   private Connection con = null;
+
+    public CuartelData() {
+        con = Conexion.getConexion();
+    }
 
 }
