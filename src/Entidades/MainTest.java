4f88484dfed6fdf30c17d4891b053017726102e2
LEsayago <lucas.e.sayago@gmail.com>
2023-10-05 19:03:08 -0300

    comit de onda

--- src/Entidades/MainTest.java
+++ src/Entidades/MainTest.java
@@ -10,9 +10,11 @@ public class MainTest {
 
         BomberoData bData = new BomberoData();
         
-        Bombero bombero = new Bombero(37000000, "Bombita Explo", "A+", LocalDate.of(1990, Month.MARCH, 23),21 ,01114565, true);
+        Bombero bombero = new Bombero(37000000, "Bombita Explo", "A+", LocalDate.of(1990, Month.MARCH, 23),21    ,01114565, true);
         System.out.println(bombero.toString());
         // se Agrega el bombero a la base de datos.
+       
+        
         bData.agregarBombero(bombero);
         
 
